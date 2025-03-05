3e98ea0059b00d25833b430634664c2d6139e24f
rodrigo792 <rodrigoalvarenga790@gmail.com>
2025-03-05 20:15:41 -0300

    Add files via upload

---
+++ algoritmo 1156/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double S = 0;
+        double numerador = 1;
+        double denominador = 1;
+
+        
+        for (int i = 0; i < 20; i++)
+        {
+            S += numerador / denominador;
+
+            
+            numerador += 2;
+            denominador *= 2;
+        }
+
+        // Exibe o resultado com 2 casas decimais
+        Console.WriteLine($"{S:F2}");
+    }
+}
+
+
---
+++ algoritmo 1156/algoritmo 1157/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+       
+        int N = int.Parse(Console.ReadLine());
+
+       
+        for (int i = 1; i <= N; i++)
+        {
+            if (N % i == 0)
+            {
+                
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
+
