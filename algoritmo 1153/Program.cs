231a2c6c1c231f107304350318cebc9f71918c01
rodrigo792 <rodrigoalvarenga790@gmail.com>
2025-03-05 20:29:07 -0300

    Add files via upload

---
+++ algoritmo 1153/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       
+        int N = int.Parse(Console.ReadLine());
+
+        int fatorial = 1;
+
+       
+        for (int i = 1; i <= N; i++)
+        {
+            fatorial *= i; 
+        }
+
+        
+        Console.WriteLine(fatorial);
+    }
+}
+
---
+++ algoritmo 1153/algoritmo 1151/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       
+        int N = int.Parse(Console.ReadLine());
+
+        
+        int[] fibonacci = new int[N];
+
+      
+        for (int i = 0; i < N; i++)
+        {
+            if (i == 0)
+                fibonacci[i] = 0;
+            else if (i == 1)
+                fibonacci[i] = 1;
+            else
+                fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+        }
+
+       
+        Console.WriteLine(string.Join(" ", fibonacci));
+    }
+}
