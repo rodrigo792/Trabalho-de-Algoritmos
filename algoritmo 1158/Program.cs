dd1a2814d4b9215b6fa48cadf4ea42f078622901
rodrigo792 <rodrigoalvarenga790@gmail.com>
2025-03-05 20:11:42 -0300

    Add files via upload

---
+++ algoritmo 1158/Program.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        
+        int N = int.Parse(Console.ReadLine());
+
+        
+        for (int i = 0; i < N; i++)
+        {
+            // Lê os valores de X e Y
+            string[] inputs = Console.ReadLine().Split();
+            int X = int.Parse(inputs[0]);
+            int Y = int.Parse(inputs[1]);
+
+            // Se X for par, começa no próximo número ímpar
+            if (X % 2 == 0)
+            {
+                X++; 
+            }
+
+            // Soma os Y ímpares consecutivos
+            int sum = 0;
+            for (int j = 0; j < Y; j++)
+            {
+                sum += X;
+                X += 2; 
+            }
+
+            
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ algoritmo 1165/Program.cs
@@ -0,0 +1,44 @@
+﻿
+using System;
+
+class Program
+{
+    
+    static bool EhPrimo(int x)
+    {
+        if (x <= 1) return false;
+        if (x == 2) return true; 
+        if (x % 2 == 0) return false; 
+
+        
+        for (int i = 3; i * i <= x; i += 2)
+        {
+            if (x % i == 0)
+                return false;
+        }
+
+        return true;
+    }
+
+    static void Main()
+    {
+        
+        int N = int.Parse(Console.ReadLine());
+
+        
+        for (int i = 0; i < N; i++)
+        {
+            int X = int.Parse(Console.ReadLine());
+
+   
+            if (EhPrimo(X))
+            {
+                Console.WriteLine($"{X} eh primo");
+            }
+            else
+            {
+                Console.WriteLine($"{X} nao eh primo");
+            }
+        }
+    }
+}
