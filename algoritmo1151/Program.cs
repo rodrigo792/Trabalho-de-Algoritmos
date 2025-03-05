7a93eb63ab297bd223c22fbcda8675e8ab4739bc
rodrigo792 <rodrigoalvarenga790@gmail.com>
2025-03-05 20:02:08 -0300

    Add files via upload

---
+++ algoritmo1151/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        int N = int.Parse(Console.ReadLine());
+
+      
+        long a = 0, b = 1;
+
+       
+        for (int i = 0; i < N; i++)
+        {
+            
+            Console.Write(a);
+
+            if (i < N - 1)
+                Console.Write(" ");
+
+           
+            long temp = a;
+            a = b;
+            b = temp + b;
+        }
+
+       
+        Console.WriteLine();
+    }
+}
