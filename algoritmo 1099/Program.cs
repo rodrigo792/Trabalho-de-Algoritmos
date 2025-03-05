971a40d316aed2aaa09ae649342939c44dc01bff
rodrigo792 <rodrigoalvarenga790@gmail.com>
2025-03-05 20:24:45 -0300

    Add files via upload

---
+++ algoritmo 1099/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        
+        int N = int.Parse(Console.ReadLine());
+
+       
+        for (int i = 0; i < N; i++)
+        {
+            
+            string[] inputs = Console.ReadLine().Split();
+            int X = int.Parse(inputs[0]);
+            int Y = int.Parse(inputs[1]);
+
+            
+            if (X > Y)
+            {
+                int temp = X;
+                X = Y;
+                Y = temp;
+            }
+
+            
+            int sum = 0;
+            for (int j = X + 1; j < Y; j++)
+            {
+                if (j % 2 != 0)  
+                {
+                    sum += j;
+                }
+            }
+
+          
+            Console.WriteLine(sum);
+        }
+    }
+}
+
+
