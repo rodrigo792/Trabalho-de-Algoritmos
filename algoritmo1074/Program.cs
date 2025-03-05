4710f2479132d86f339061dbf8c2376a6021da20
rodrigo792 <rodrigoalvarenga790@gmail.com>
2025-03-05 20:07:42 -0300

    Add files via upload

---
+++ algoritmo1074/Program.cs
@@ -0,0 +1,46 @@
+﻿
+using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        // Ler a quantidade de valores N
+        int n = int.Parse(Console.ReadLine());
+
+        // Processar cada valor
+        for (int i = 0; i < n; i++)
+        {
+            int x = int.Parse(Console.ReadLine());
+
+            if (x == 0)
+            {
+                Console.WriteLine("NULL");
+            }
+            else if (x % 2 == 0) 
+            {
+                if (x > 0)
+                {
+                    Console.WriteLine("EVEN POSITIVE");
+                }
+                else
+                {
+                    Console.WriteLine("EVEN NEGATIVE");
+                }
+            }
+            else 
+            {
+                if (x > 0)
+                {
+                    Console.WriteLine("ODD POSITIVE");
+                }
+                else
+                {
+                    Console.WriteLine("ODD NEGATIVE");
+                }
+            }
+        }
+    }
+}
+
+
---
+++ algoritmo1079/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+      
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            
+            string[] valores = Console.ReadLine().Split(' ');
+            double valor1 = double.Parse(valores[0], CultureInfo.InvariantCulture);
+            double valor2 = double.Parse(valores[1], CultureInfo.InvariantCulture);
+            double valor3 = double.Parse(valores[2], CultureInfo.InvariantCulture);
+
+           
+            double mediaPonderada = (valor1 * 2 + valor2 * 3 + valor3 * 5) / 10;
+
+           
+            Console.WriteLine(mediaPonderada.ToString("F1", CultureInfo.InvariantCulture));
+        }
+    }
+}
+
